f17fddb8612b35bade241001af6feb3e69c4e86b
Natanael <nbdb2005@gmail.com>
2025-06-09 20:32:43 -0300

    Bomba

--- src/middlewares/checkRole.js
+++
@@ -1,8 +0,0 @@
-export const checkRole = (role) => {
-  return (req, res, next) => {
-    if (req.user.role !== role) {
-      return res.status(403).json({ message: 'Acesso negado: permissão insuficiente' });
-    }
-    next();
-  };
-};
