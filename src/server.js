63ab4a3f8c8f4c6abe8c8e12131f6d90ba9ab40f
Natanael <nbdb2005@gmail.com>
2025-06-11 18:18:30 -0300

    fix: server.js atualizado

--- src/server.js
+++ src/server.js
@@ -16,9 +16,9 @@ sequelize.authenticate()
   .then(() => console.log('Conexão com o banco de dados estabelecida com sucesso.'))
   .catch(err => console.error('Não foi possível conectar ao banco de dados:', err));
 
-sequelize.sync()
-  .then(() => console.log('Modelos sincronizados com o banco de dados.'))
-  .catch(err => console.error('Erro ao sincronizar modelos:', err));
+// sequelize.sync()
+//   .then(() => console.log('Modelos sincronizados com o banco de dados.'))
+//   .catch(err => console.error('Erro ao sincronizar modelos:', err));
 
 app.use('/api', router);
 
